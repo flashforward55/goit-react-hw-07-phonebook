050479355e57b500cfa71616b76eba3ac303aba5
Anton Styazhkin <rudana089@gmail.com>
2023-07-12 22:14:30 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -4,34 +4,34 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 axios.defaults.baseURL = 'https://64ac4c9c9edb4181202f6743.mockapi.io';
 
 export const fetchContacts = createAsyncThunk("phonebook/fetchAll",
-  async (_, thunkApi) => {
+  async (_, { rejectWithValue }) => {
     try {
       const contacts = await axios.get('/contacts');
       return contacts.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error);
+      throw rejectWithValue(error);
     };
   }
 );
 
 export const addContact = createAsyncThunk("phonebook/addContact",
-  async ({ name, phone, id }, thunkApi) => {
+  async ({ name, phone, id }, { rejectWithValue }) => {
     try {
       const contacts = await axios.post('/contacts', { name, phone, id });
       return contacts.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error);
+      throw rejectWithValue(error);
     };
   }
 );
 
 export const deleteContact = createAsyncThunk("phonebook/deleteContact",
-  async (id, thunkApi) => {
+  async (id, { rejectWithValue }) => {
     try {
       const contacts = await axios.delete(`/contacts/${id}`);
       return contacts.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error);
+      throw rejectWithValue(error);
     };
   }
 );
